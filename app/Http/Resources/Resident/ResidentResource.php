857d4ad15a9eb3dd3cd42f53d37b812bfab0bb50
RIOKOWI <achyarrio2004@gmail.com>
2026-09-01 20:48:11 +0700

    feat(resident): create resident resource

---
+++ app/Http/Resources/Resident/ResidentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Resident;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ResidentResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nik' => $this->nik,
+            'full_name' => $this->full_name,
+            'birth_place' => $this->birth_place,
+            'birth_date' => $this->birth_date?->format('Y-m-d'),
+            'gender' => $this->gender,
+            'religion' => $this->religion,
+            'occupation' => $this->occupation,
+            'marital_status' => $this->marital_status,
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'status' => $this->status,
+        ];
+    }
+}
