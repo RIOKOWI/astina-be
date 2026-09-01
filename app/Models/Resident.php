9f9a9658663904474218da3e0882aad50ecbd1e3
RIOKOWI <achyarrio2004@gmail.com>
2026-09-01 14:53:19 +0700

    feat(model): add resident

---
+++ app/Models/Resident.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Resident extends Model
+{
+    protected $fillable = [
+        'nik',
+        'no_kk',
+        'full_name',
+        'birth_place',
+        'birth_date',
+        'gender',
+        'religion',
+        'marital_status',
+        'occupation',
+        'phone',
+        'email',
+        'status',
+        'joined_at',
+        'left_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'birth_date' => 'date',
+            'joined_at' => 'date',
+            'left_at' => 'date',
+        ];
+    }
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function households(): BelongsToMany
+    {
+        return $this->belongsToMany(Household::class, 'resident_households')
+            ->withPivot('relationship', 'joined_at', 'left_at', 'is_current')
+            ->withTimestamps();
+    }
+
+    public function dueBills(): HasMany
+    {
+        return $this->hasMany(DueBill::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function complaints(): HasMany
+    {
+        return $this->hasMany(Complaint::class);
+    }
+
+    public function letters(): HasMany
+    {
+        return $this->hasMany(Letter::class);
+    }
+}
