493edf17e3d4c2421bf1c2554333b1e309290ded
RIOKOWI <achyarrio2004@gmail.com>
2026-09-01 14:53:16 +0700

    feat(model): add lettertype

---
+++ app/Models/LetterType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class LetterType extends Model
+{
+    protected $fillable = [
+        'code',
+        'name',
+        'description',
+        'template_path',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_active' => 'boolean',
+        ];
+    }
+
+    public function fields(): HasMany
+    {
+        return $this->hasMany(LetterField::class);
+    }
+
+    public function letters(): HasMany
+    {
+        return $this->hasMany(Letter::class);
+    }
+}
