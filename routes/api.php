f9221f6dc1bddf8aa17cc30480d733f6a1f78c25
RIOKOWI <achyarrio2004@gmail.com>
2026-09-01 20:48:06 +0700

    feat(api): register api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\HouseholdController;
+use App\Http\Controllers\Api\MeController;
+use App\Http\Controllers\Api\ResidentController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
@@ -11,5 +14,16 @@ Route::prefix('v1')->group(function () {
             Route::get('/me', [AuthController::class, 'me']);
             Route::post('/logout', [AuthController::class, 'logout']);
         });
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me/resident', [MeController::class, 'resident']);
+        Route::get('/me/household', [MeController::class, 'household']);
+
+        Route::get('/residents', [ResidentController::class, 'index']);
+        Route::get('/residents/{resident}', [ResidentController::class, 'show']);
+
+        Route::get('/households', [HouseholdController::class, 'index']);
+        Route::get('/households/{household}', [HouseholdController::class, 'show']);
     });
 });
