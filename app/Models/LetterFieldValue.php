e9c76cf598b9e85e5513e1a3b572a72efd6af6f2
RIOKOWI <achyarrio2004@gmail.com>
2026-09-01 14:53:16 +0700

    feat(model): add letterfieldvalue

---
+++ app/Models/LetterFieldValue.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LetterFieldValue extends Model
+{
+    protected $fillable = [
+        'letter_id',
+        'letter_field_id',
+        'value',
+    ];
+
+    public function letter(): BelongsTo
+    {
+        return $this->belongsTo(Letter::class);
+    }
+
+    public function letterField(): BelongsTo
+    {
+        return $this->belongsTo(LetterField::class);
+    }
+}
