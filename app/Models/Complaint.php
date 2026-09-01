9988a12f847db5ed82db6df898adc2f5ad3ec78c
RIOKOWI <achyarrio2004@gmail.com>
2026-09-02 02:24:54 +0700

    feat(model): update complaint model

--- app/Models/Complaint.php
+++ app/Models/Complaint.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Complaint extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'resident_id',
         'assigned_to',
