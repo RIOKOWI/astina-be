51c040a91d7b3a66b3ccb3ef4ec9abc9c4601e86
RIOKOWI <achyarrio2004@gmail.com>
2026-09-02 19:40:09 +0700

    test(stamp): add stamp factory

---
+++ database/factories/StampFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Letter;
+use App\Models\Stamp;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Stamp>
+ */
+class StampFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'letter_id' => Letter::factory(),
+            'stamped_by' => User::factory(),
+            'stamp_path' => 'stamps/' . fake()->uuid() . '.png',
+            'stamped_at' => now(),
+            'created_at' => now(),
+        ];
+    }
+}
