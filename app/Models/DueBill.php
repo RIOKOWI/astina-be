0b28cbdf1f947f9d317a3066f628ca393df74c9b
RIOKOWI <achyarrio2004@gmail.com>
2026-09-02 17:02:42 +0700

    feat(duebill): add hasfactory trait

--- app/Models/DueBill.php
+++ app/Models/DueBill.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class DueBill extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'due_id',
         'resident_id',
