f49b40349a0ad22b88f4404a85463a82128ca1c2
RIOKOWI <achyarrio2004@gmail.com>
2026-09-02 19:39:50 +0700

    feat(letter): add hasfactory trait

--- app/Models/Letter.php
+++ app/Models/Letter.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Letter extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'reference_no',
         'letter_type_id',
