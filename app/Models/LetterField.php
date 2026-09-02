4e84435a83ddaf27a6df3ed547acf4b6b155b76d
RIOKOWI <achyarrio2004@gmail.com>
2026-09-02 19:39:51 +0700

    feat(letterfield): add hasfactory trait

--- app/Models/LetterField.php
+++ app/Models/LetterField.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LetterField extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'letter_type_id',
         'field_key',
