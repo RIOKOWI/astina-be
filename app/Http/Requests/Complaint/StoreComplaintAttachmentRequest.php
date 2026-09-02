49385f4a7e28c5ba8494094eca39efaba7097336
RIOKOWI <achyarrio2004@gmail.com>
2026-09-02 11:17:22 +0700

    feat(complaint): allow residents to upload attachments

--- app/Http/Requests/Complaint/StoreComplaintAttachmentRequest.php
+++ app/Http/Requests/Complaint/StoreComplaintAttachmentRequest.php
@@ -8,7 +8,7 @@ class StoreComplaintAttachmentRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return $this->user()?->hasRole('rt') ?? false;
+        return $this->user() !== null;
     }
 
     public function rules(): array
