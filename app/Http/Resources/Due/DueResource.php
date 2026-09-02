5e0aa946e9d6c972c793332286862939fdadb3b0
RIOKOWI <achyarrio2004@gmail.com>
2026-09-02 17:02:53 +0700

    feat(due): add due resource

---
+++ app/Http/Resources/Due/DueResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Due;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DueResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'amount' => (int) $this->amount,
+            'frequency' => $this->frequency,
+            'start_date' => $this->start_date?->format('Y-m-d'),
+            'end_date' => $this->end_date?->format('Y-m-d'),
+            'is_active' => (bool) $this->is_active,
+            'created_at' => $this->created_at?->toIso8601String(),
+        ];
+    }
+}
