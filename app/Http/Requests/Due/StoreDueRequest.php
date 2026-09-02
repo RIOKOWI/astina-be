7e81ec503a6ba6a90ba2b6a50dcab0a80c63df4f
RIOKOWI <achyarrio2004@gmail.com>
2026-09-02 17:02:49 +0700

    feat(due): add store due request

---
+++ app/Http/Requests/Due/StoreDueRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Due;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDueRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()?->hasRole('rt') ?? false;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string', 'max:1000'],
+            'amount' => ['required', 'integer', 'min:100'],
+            'frequency' => ['nullable', 'in:monthly,quarterly,yearly,one-time'],
+            'start_date' => ['nullable', 'date'],
+            'end_date' => ['nullable', 'date', 'after_or_equal:start_date'],
+            'is_active' => ['nullable', 'boolean'],
+        ];
+    }
+}
