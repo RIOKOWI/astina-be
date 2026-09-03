1f1ead03a161397e25c3aa339f85f47c318e4849
RIOKOWI <achyarrio2004@gmail.com>
2026-09-03 19:08:15 +0700

    feat(media): add collection constants for ktp and kk

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Media extends Model
 {
+    use HasFactory;
+
+    public const COLLECTION_KTP = 'ktp';
+
+    public const COLLECTION_KK = 'kk';
+
     protected $fillable = [
         'model_type',
         'model_id',
