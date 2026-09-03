dde890ac3fc8932c9791f060110d20e5e6b555fe
RIOKOWI <achyarrio2004@gmail.com>
2026-09-03 15:19:06 +0700

    feat(seeder): create seeder for activity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             AssetSeeder::class,
             ComplaintSeeder::class,
             FinanceSeeder::class,
+            ActivitySeeder::class,
             LetterSeeder::class,
         ]);
     }
