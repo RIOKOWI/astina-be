2850ebe0275bf807c64100caf6552c8b3841d9aa
RIOKOWI <achyarrio2004@gmail.com>
2026-09-04 10:27:31 +0700

    feat(config): add letters configuration

---
+++ config/letters.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'templates' => [
+        'surat_pengantar' => 'letters/templates/surat-pengantar/Surat_Pengantar_RT005_RW016.docx',
+    ],
+
+    'default_signature' => 'letters/templates/sign/tanda-tangan.png',
+
+    'default_stamp' => 'letters/templates/stamp/stempel.png',
+
+    'generated_path' => 'letters/generated',
+];
