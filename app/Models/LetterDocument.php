7cf0ba7a0c1e9f7075c2d83b68d49fc15e56c7d3
RIOKOWI <achyarrio2004@gmail.com>
2026-09-05 06:55:58 +0700

    style(letterdocument): fix formatting

--- app/Models/LetterDocument.php
+++ app/Models/LetterDocument.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class LetterDocument extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'letter_id',
         'document_type',
