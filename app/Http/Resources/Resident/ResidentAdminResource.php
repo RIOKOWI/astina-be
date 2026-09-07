a7c8f95b618576e30b79213eed8d9a45401e8564
RIOKOWI <achyarrio2004@gmail.com>
2026-09-07 19:15:48 +0700

    feat(resident): update resident admin resource

--- app/Http/Resources/Resident/ResidentAdminResource.php
+++ app/Http/Resources/Resident/ResidentAdminResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Resident;
 
+use App\Models\Media;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,27 +13,28 @@ class ResidentAdminResource extends JsonResource
         $data = (new ResidentResource($this))->toArray($request);
 
         $data['household'] = null;
+        $currentHousehold = null;
         if ($this->relationLoaded('households') && $this->households->isNotEmpty()) {
-            $household = $this->households->first();
+            $currentHousehold = $this->households->first();
             $data['household'] = [
-                'id' => $household->id,
-                'no_kk' => $household->no_kk,
-                'address' => $household->address,
-                'rt' => $household->rt,
-                'rw' => $household->rw,
-                'postal_code' => $household->postal_code,
-                'status' => $household->status,
+                'id' => $currentHousehold->id,
+                'no_kk' => $currentHousehold->no_kk,
+                'address' => $currentHousehold->address,
+                'rt' => $currentHousehold->rt,
+                'rw' => $currentHousehold->rw,
+                'postal_code' => $currentHousehold->postal_code,
+                'status' => $currentHousehold->status,
             ];
         } elseif ($this->relationLoaded('households') === false && $this->headedHousehold) {
-            $household = $this->headedHousehold;
+            $currentHousehold = $this->headedHousehold;
             $data['household'] = [
-                'id' => $household->id,
-                'no_kk' => $household->no_kk,
-                'address' => $household->address,
-                'rt' => $household->rt,
-                'rw' => $household->rw,
-                'postal_code' => $household->postal_code,
-                'status' => $household->status,
+                'id' => $currentHousehold->id,
+                'no_kk' => $currentHousehold->no_kk,
+                'address' => $currentHousehold->address,
+                'rt' => $currentHousehold->rt,
+                'rw' => $currentHousehold->rw,
+                'postal_code' => $currentHousehold->postal_code,
+                'status' => $currentHousehold->status,
             ];
         }
 
@@ -50,6 +52,39 @@ class ResidentAdminResource extends JsonResource
                     ])
                     : [],
             ];
+        }
+
+        $data['ktp'] = null;
+        if ($this->relationLoaded('media')) {
+            $ktp = $this->media->firstWhere('collection', Media::COLLECTION_KTP);
+            if ($ktp) {
+                $data['ktp'] = [
+                    'exists' => true,
+                    'mime_type' => $ktp->mime_type,
+                    'file_size' => $ktp->file_size,
+                    'file_name' => $ktp->file_name,
+                    'url' => "/api/v1/residents/{$this->id}/documents/ktp/file",
+                ];
+            } else {
+                $data['ktp'] = ['exists' => false];
+            }
+        }
+
+        $data['kk'] = null;
+        if ($currentHousehold && $currentHousehold->relationLoaded('media')) {
+            $kk = $currentHousehold->media->firstWhere('collection', Media::COLLECTION_KK);
+            if ($kk) {
+                $data['kk'] = [
+                    'exists' => true,
+                    'no_kk' => $currentHousehold->no_kk,
+                    'mime_type' => $kk->mime_type,
+                    'file_size' => $kk->file_size,
+                    'file_name' => $kk->file_name,
+                    'url' => "/api/v1/residents/{$this->id}/documents/kk/file",
+                ];
+            } else {
+                $data['kk'] = ['exists' => false];
+            }
         }
 
         return $data;
