b64dfa6ce3de1e45c8f59d6752395eca8a444085
RIOKOWI <achyarrio2004@gmail.com>
2026-09-07 19:15:49 +0700

    feat(household): update resident household model

--- app/Models/ResidentHousehold.php
+++ app/Models/ResidentHousehold.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ResidentHousehold extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'resident_id',
         'household_id',
