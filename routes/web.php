badffeecd16e24338eb75041bb809be00a5e354f
RIOKOWI <achyarrio2004@gmail.com>
2026-09-10 18:21:58 +0700

    feat(routes): add dev letter preview routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Dev\LetterPreviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+if (app()->environment(['local', 'testing'])) {
+    Route::prefix('dev/letters')->group(function () {
+        Route::get('/surat-pengantar', [LetterPreviewController::class, 'show']);
+        Route::get('/surat-pengantar/reference', [LetterPreviewController::class, 'reference']);
+        Route::get('/surat-pengantar/compare', [LetterPreviewController::class, 'compare']);
+    });
+}
