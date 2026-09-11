1640e13b26ecf0f39d325bad93bf9b9ee7df8aa6
RIOKOWI <achyarrio2004@gmail.com>
2026-09-11 13:09:09 +0700

    refactor(letter): simplify signature and stamp paths in config

--- config/letters.php
+++ config/letters.php
@@ -5,9 +5,9 @@ return [
         'surat_pengantar' => 'letters/templates/surat-pengantar/Surat_Pengantar_RT005_RW016.docx',
     ],
 
-    'default_signature' => 'letters/templates/sign/tanda-tangan.png',
+    'default_signature' => 'sign/tanda-tangan.png',
 
-    'default_stamp' => 'letters/templates/stamp/stempel.png',
+    'default_stamp' => 'stamp/stempel.png',
 
     'generated_path' => 'letters/generated',
 ];
